78befcafa0b4e0e091ec448c67627664a0e0110f
Sarah Parker <sarahlparker4@gmail.com>
2025-08-03 10:45:45 -0400

    changed database url to reflect render database

--- api/database.py
+++ api/database.py
@@ -3,12 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from fastapi import Depends 
+from config import DATABASE_URL
 
 # Use your actual DB URL here (e.g., PostgreSQL or SQLite)
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Carol2024%23@localhost:5432/connectehr"
+DATABASE_URL = DATABASE_URL
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
